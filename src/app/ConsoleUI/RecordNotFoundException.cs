8aaa6e614bc8ac5519ba0d4a610938c2c655647f
Omar Ali Sheikh-Omar <omar@keepfocus.dk>
2015-05-05 15:56:39 +0200

    Add RecordNotFoundException.

---
+++ src/app/ConsoleUI/RecordNotFoundException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleUI
+{
+    public class RecordNotFoundException : Exception
+    {
+        public RecordNotFoundException(string type, int id) :
+            base(String.Format("{0} with the ID {1} was not found.", type, id))
+        {
+            
+        }
+    }
+}

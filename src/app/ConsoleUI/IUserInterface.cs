8f9b3fb12dd989317b89bd5ed790db6bc45388b3
Omar Ali Sheikh <sheikhomar@gmail.com>
2015-05-05 19:56:07 +0200

    Extract UI actions into an interface.

---
+++ src/app/ConsoleUI/IUserInterface.cs
@@ -0,0 +1,18 @@
+﻿using ostrich.ConsoleUI;
+
+namespace ConsoleUI
+{
+    public interface IUserInterface
+    {
+        void DisplayUserNotFound();
+        void DisplayProductNotFound();
+        void DisplayUserInfo();
+        void DisplayTooManyArgumentsError();
+        void DisplayAdminCommandNotFoundMessage();
+        void DisplayUserBuysProduct(BuyTransaction transaction);
+        void DisplayUserBuysProduct(int count);
+        void Close();
+        void DisplayInsufficientCash();
+        void DisplayGeneralError(string errorString);
+    }
+}

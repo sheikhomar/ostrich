d8d7855cb51a8c1d10042f30c75a72c22412b758
Omar Ali Sheikh <sheikhomar@gmail.com>
2015-05-07 15:26:44 +0200

    Null checks.

--- src/app/Core/Exceptions/InsufficientCreditsException.cs
+++ src/app/Core/Exceptions/InsufficientCreditsException.cs
@@ -5,13 +5,24 @@ namespace ostrich.Core.Exceptions
     public class InsufficientCreditsException : Exception
     {
         public InsufficientCreditsException(User user, Product product)
-            : base(String.Format("User '{0}' has insufficient funds to buy product '{1}'", user.UserID, product.ProductID))
         {
+            if (user == null) 
+                throw new ArgumentNullException("user");
+            if (product == null) 
+                throw new ArgumentNullException("product");
+
             User = user;
             Product = product;
         }
 
         public User User { get; private set; }
         public Product Product { get; private set; }
+        public override string Message
+        {
+            get
+            {
+                return String.Format("User '{0}' has insufficient funds to buy product '{1}'", User.UserID, Product.ProductID);
+            }
+        }
     }
 }

4f7b219bb67dd5ca3bf6f9d6cf9470c1897d678d
Omar Ali Sheikh <sheikhomar@gmail.com>
2015-05-07 16:29:42 +0200

    Add tests for SeasonalProduct.

--- src/app/Core/SeasonalProduct.cs
+++ src/app/Core/SeasonalProduct.cs
@@ -4,25 +4,46 @@ namespace ostrich.Core
 {
     public class SeasonalProduct : Product
     {
-        private bool active;
+        private DateTime? seasonEndsAt;
+        private DateTime? seasonStartsAt;
 
         public SeasonalProduct(int productId, string name, int price)
             : base(productId, name, price)
         {
         }
 
-        public DateTime? SeasonStartsAt { get; set; }
-        public DateTime? SeasonEndsAt { get; set; }
+        public DateTime? SeasonStartsAt
+        {
+            get { return seasonStartsAt; }
+            set
+            {
+                if (SeasonEndsAt < value)
+                    throw new InvalidOperationException("Start date must be before end date");
+                seasonStartsAt = value;
+            }
+        }
+
+        public DateTime? SeasonEndsAt
+        {
+            get { return seasonEndsAt; }
+            set
+            {
+                if (SeasonStartsAt > value)
+                    throw new InvalidOperationException("End date must be after start date");
+                seasonEndsAt = value;
+            }
+        }
 
         public override bool Active
         {
-            get { return active; }
-            set
+            get
             {
-                // TODO: Doesn't make any sense to set the Active property
-                throw new NotImplementedException("Doesn't make any sense.");
-                active = value;
+                DateTime now = DateTime.Now;
+                bool seasonBegan = SeasonStartsAt == null || now >= SeasonStartsAt;
+                bool seasonNotEnded = SeasonEndsAt == null || now <= SeasonEndsAt;
+                return seasonBegan && seasonNotEnded;
             }
+            set { throw new InvalidOperationException("Active property cannot be set for seasonal products."); }
         }
     }
 }
---
+++ src/test/Tests/SeasonalProductTests.cs
@@ -0,0 +1,73 @@
+using System;
+using NUnit.Framework;
+using ostrich.Core;
+
+namespace ostrich.Tests
+{
+    [TestFixture]
+    public class SeasonalProductTests
+    {
+        private SeasonalProduct product;
+
+        [SetUp]
+        public void Setup()
+        {
+            product = new SeasonalProduct(1, "Party food", 15000);
+        }
+
+        [Test]
+        public void Active_should_return_true_if_season_has_not_been_specified()
+        {
+            Assert.IsTrue(product.Active);
+        }
+
+        [Test]
+        public void Active_should_return_true_if_SeasonEndsAt_is_in_the_future()
+        {
+            product.SeasonEndsAt = DateTime.Now.AddDays(1);
+            
+            Assert.IsTrue(product.Active);
+        }
+
+        [Test]
+        public void Active_should_return_false_if_season_has_ended()
+        {
+            product.SeasonEndsAt = DateTime.Now.AddDays(-1);
+
+            Assert.IsFalse(product.Active);
+        }
+
+        [Test]
+        public void Active_should_return_false_if_season_has_not_started()
+        {
+            product.SeasonStartsAt = DateTime.Now.AddDays(1);
+            product.SeasonEndsAt = DateTime.Now.AddDays(2);
+
+            Assert.IsFalse(product.Active);
+        }
+
+        [Test]
+        public void Active_should_return_true_if_started_season_does_not_end()
+        {
+            product.SeasonStartsAt = DateTime.Now.AddDays(-1);
+
+            Assert.IsTrue(product.Active);
+        }
+
+        [Test]
+        public void SeasonEndsAt_must_come_after_SeasonStartsAt()
+        {
+            product.SeasonStartsAt = DateTime.Now;
+
+            Assert.Throws<InvalidOperationException>(() => product.SeasonEndsAt = DateTime.Now.AddDays(-1));
+        }
+
+        [Test]
+        public void SeasonStartsAt_must_come_be_before_SeasonEndsAt()
+        {
+            product.SeasonEndsAt = DateTime.Now;
+
+            Assert.Throws<InvalidOperationException>(() => product.SeasonStartsAt = DateTime.Now.AddDays(1));
+        }
+    }
+}

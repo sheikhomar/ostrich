5cf28b50f60da851a7bb61c677bce9630aa23188
Omar Ali Sheikh <sheikhomar@gmail.com>
2015-05-07 15:32:06 +0200

    Refactor to simply code.

--- src/app/Core/Exceptions/UserNotFoundException.cs
+++ src/app/Core/Exceptions/UserNotFoundException.cs
@@ -5,14 +5,15 @@ namespace ostrich.Core.Exceptions
     public class UserNotFoundException : Exception
     {
         public UserNotFoundException(string userName)
-            : base(String.Format("User '{0}' was not found.", userName))
         {
-            if (string.IsNullOrWhiteSpace(userName)) 
-                throw new ArgumentNullException("userName");
-
             UserName = userName;
         }
 
         public string UserName { get; private set; }
+
+        public override string Message
+        {
+            get { return String.Format("User '{0}' was not found.", UserName); }
+        }
     }
 }

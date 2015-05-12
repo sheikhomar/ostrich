52febfce27f717167e00d14a2b9739dd36d6a124
Omar Ali Sheikh <sheikhomar@gmail.com>
2015-05-12 20:29:15 +0200

    Extract to methods.

--- src/app/ConsoleUI/Program.cs
+++ src/app/ConsoleUI/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Text;
 using ostrich.Core;
 
@@ -8,21 +9,50 @@ namespace ostrich.ConsoleUI
     {
         static void Main(string[] args)
         {
-            var reader = new StreamReader("products.csv", Encoding.GetEncoding(1252));
-            var importer = new ProductCatalogImporter(reader);
-            var productCatalog = importer.Import();
-
-            ITransactionStore transactionFile = new TransactionFileStore("transactions.log");
-            var transactions = new TransactionManager(transactionFile);
-
-            var userRepository = new UserRepository();
-            userRepository.Add(new User(1, "Joakim", "Von And", "b") { Balance = int.MaxValue });
-            userRepository.Add(new User(2, "Anders", "And", "a") { Balance = 6000 });
+            var productCatalog = GetProductCatalog();
+            var transactions = GetTransactionManager();
+            var userRepository = GetUserRepository();
 
             IBackendSystem backendSystem = new DefaultBackendSystem(productCatalog, userRepository, transactions);
             CommandLineInterface cli = new CommandLineInterface();
             CommandParser parser = new CommandParser(cli, backendSystem);
             cli.Start(parser);
+        }
+
+        private static UserRepository GetUserRepository()
+        {
+            var userRepository = new UserRepository();
+            userRepository.Add(new User(1, "Joakim", "Von And", "b") {Balance = int.MaxValue});
+            userRepository.Add(new User(2, "Anders", "And", "a") {Balance = 6000});
+            return userRepository;
+        }
+
+        private static ProductCatalog GetProductCatalog()
+        {
+            var reader = new StreamReader("products.csv", Encoding.GetEncoding(1252));
+            var importer = new ProductCatalogImporter(reader);
+            var productCatalog = importer.Import();
+            productCatalog.Add(new SeasonalProduct(2000, "Forårsfest")
+            {
+                SeasonStartsAt = new DateTime(2015, 4, 1),
+                SeasonEndsAt = new DateTime(2015, 7, 15),
+                CanBeBoughtOnCredit = true,
+                Price = 14900
+            });
+            productCatalog.Add(new SeasonalProduct(2001, "Sommerfest")
+            {
+                SeasonStartsAt = new DateTime(2015, 7, 15),
+                SeasonEndsAt = new DateTime(2015, 8, 1),
+                Price = 29900
+
+            });
+            return productCatalog;
+        }
+
+        private static TransactionManager GetTransactionManager()
+        {
+            ITransactionStore transactionFile = new TransactionFileStore("transactions.log");
+            return new TransactionManager(transactionFile);
         }
     }
 }
